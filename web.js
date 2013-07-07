0a95940f5f1bb589a5fb8fbdd2bedd3e3ed068ae
Sunil <sunnydayzrback@gmail.com>
2013-07-07 23:13:17 +0000

    sending buffer out

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fileContent = fs.readFileSync('index.html').toString('utf-8');
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(fileContent);
 });
 
 var port = process.env.PORT || 5000;
