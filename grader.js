e823bddda34e43e77997a56041600a29a6495278
Sunil <sunnydayzrback@gmail.com>
2013-07-11 04:32:36 +0000

    Updated grader.js to validate urls against json files as well.

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://safe-shelf-8421.herokuapp.com/";
+
 
 //returns file name if file exists otherwise logs error
  var assertFileExists = function(infile) {
@@ -38,10 +41,28 @@ var CHECKSFILE_DEFAULT = "checks.json";
     return instr;
 };
 
-//cheerio is like beautiful soup in python. Allows us to access the elements via api
+/*
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+ var assertHTMLExists = function(inurl) {
+  rest.get(inurl).on('complete', function(result) {
+    if (result instanceof Error) {
+      console.log('Error: ' + result.message);
+      process.exit(1); 
+    } else {
+       var instr;
+    }
+  });    
+}; 
+
+
+assertHTMLExists('www.yahoo.com');
+
+*/
+
+//cheerio is like beautiful soup in python. Allows us to access the dom elements via api
+
+var cheerioHtmlFile = function(input) {
+   return cheerio.load(fs.readFileSync(input));
 };
 
 //json.parse convets a json string to a javascript object. Returns an object or an array.
@@ -50,18 +71,38 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(input, checksfile, key) {
+    if (key == 'f') //if it is a file, load from file.
+        $ = cheerioHtmlFile(input);
+    else //if not, it is html code
+        $ = cheerio.load(input);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     //javascript supports java style for loop and python style for loop as well
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+       //The checks array will contain information regarding the dom object, its prev, next and the parent. To see this output uncomment the console.log line below.  
+       var present = $(checks[ii]).length > 0;
+       //console.log($(checks[ii]));
        //dictionary from array  
        out[checks[ii]] = present;
     }
     return out;
 };
+
+
+// helper function for url, uses rest api. 
+var checkURL = function(url, checksfile) {
+   rest.get(url).on('complete', function(result) {
+    if (result instanceof Error) {
+      console.log('Error: ' + result.message);
+      process.exit(1); 
+    } else {
+        var checkJson = checkHtmlFile(result, checksfile);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
+  });
+} 
 
 /* clones a function. this way we can add data to the second function without affecting the first function. see the example below. 
 
@@ -83,12 +124,17 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_html>', 'URL of the index file')
         .parse(process.argv);
 
-
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    // takes the html file and json file and 
+    if (program.url){
+      var checkJson = checkURL(program.url, program.checks);
+    } else {    
+      var checkJson = checkHtmlFile(program.file, program.checks,'f');    
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
